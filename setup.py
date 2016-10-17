477db2e2397c223b6a978272717b3c5cab8dff45
Mike Charles <mike.charles@noaa.gov>
2016-10-17 16:01:54 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ requirements = ['pyyaml', 'numpy', 'scipy', 'basemap', 'matplotlib', 'cpc.geogri
 
 setup(
     name="cpc.geoplot",
-    version='v0.1.1',
+    version='v0.1.2',
     description="CPC geospatial plotting",
     long_description=readme + '\n\n' + history,
     author="Mike Charles",
